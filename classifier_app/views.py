c3cfda62295191c9c07a3d0cf662ea6b3307c505
Andrew Musicant <Andrewmusicant@yahoo.com>
2016-09-01 17:28:03 -0400

    Updated views

--- classifier_app/views.py
+++ classifier_app/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from rest_framework import viewsets
+from .model import Classifier, Data
+from .serializers import ClassifierSerializer, DataSerializer
 
 
 # Create your views here.
 class IndexView(View):
     def get(self, request, *args, **kwargs):
         return HttpResponse('Hello, World!')
+
+
+class ClassifierViewSet(viewsets.ModelViewSet):
+    serializer_class = ClassifierSerializer
+    queryset = Classifier.objects.all()
+
+
+class DataViewSet(viewsets.ModelViewSet):
+    serializer_class = DataSerializer
+    queryset = Data.objects.all()
