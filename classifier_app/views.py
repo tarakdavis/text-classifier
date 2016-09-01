9ca605744e1509302680638f9b082d81205361e1
ArtyomMinsk <artem.nesterenko@gmail.com>
2016-09-01 17:28:51 -0400

    Added urls

--- classifier_app/views.py
+++ classifier_app/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.views import View
+from .serializers import QuestionSerializer, AnswerSerializer, CommentSerializer
+from rest_framework import viewsets
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Classifier
+from django.contrib.auth.views import login
 
 
+# def index(request):
+#     return HttpResponse('sup DAWG')
 # Create your views here.
 class IndexView(View):
     def get(self, request, *args, **kwargs):
