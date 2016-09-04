be3088ea12bf8ffa8175ceb58e49a0122fce2c71
T. Robert Ward <trironyard@icloud.com>
2016-09-04 18:50:24 -0400

    BIG commit of changes

--- classifier_app/views.py
+++ classifier_app/views.py
@@ -6,74 +6,113 @@ from .models import Classifier, Data
 from .serializers import ClassifierSerializer, DataSerializer
 from django.views.generic import TemplateView
 from django.views import generic
+from django.urls import reverse
+
+from rest_framework.decorators import api_view
+
 # for the Pipeline
 from sklearn.feature_extraction.text import CountVectorizer as CV
 from sklearn.feature_extraction.text import TfidfTransformer as TF
 from sklearn.naive_bayes import MultinomialNB as MNB
 from sklearn.pipeline import Pipeline
+from collections import defaultdict
 
 
-def pipeline_predict(request, pk):  # self, X_train, y_train, label, X_test, y_test
-    # self.X_train = X_train  # comes from DB
-    # self.y_train = y_train  # passed in ( calc'd first )
-    # self.label = label      # comes from user input
-    # self.X_test = X_test    # comes from user input
-    # self.y_test = y_test    # passed in ( calc'd first )
-    classifier = Classifier.object.get(id=2)
+
+def index(request):
+    return render(request, 'classifier_app/index.html')
+
+
+def train(request):
+    return render(request, 'classifier_app/train.html')
+
+
+def predict(request):
+    return render(request, 'classifier_app/predict.html')
+
+
+# def new_classif(request):
+#     if request.method == 'POST':
+#         preclas = request.POST.get('newClass')
+#
+#
+# def new_train(request):
+#     if request.method == 'POST':
+#         preclas = request.POST.get('newClass')
+#         precat = request.POST.get('newCat')
+#         pretext = request.POST.get('newText')
+#         print("===========================", preclas, precat, pretext)
+#
+#     context = {'response': "preclas" + "precat" + "pretext"}
+#     return render(request, 'classifier_app/tindex.html', context)
+
+
+def pipeline_predict(request):
+    if request.method == 'POST':
+        preTest = request.POST.get('text_to_classif')
+        predictTestData = preTest.split()
+        print("*********************", predictTestData)
     py_pipeline = Pipeline([("count", CV()), ("tfid", TF()), ("multi", MNB())])
-    data_set = classifier.data_set.all()
-    py_pipeline.fit()
-    prediction = py_pipeline.predict('hola')
-    score = py_pipeline.score(X_test, y_test)
-    return prediction, score
-# def index(request):
-#     return HttpResponse('hello world')
+    dbData = Data.objects.all()
+    X_language_train = []
+    y_language_train = []
+    for each in dbData:
+        xlt = each.text.split(", ")
+        ylt = len(each.text.split(", "))*[each.category]
+        X_language_train.extend(xlt)
+        y_language_train.extend(ylt)
+    print(X_language_train, y_language_train)
+    py_pipeline.fit(X_language_train, y_language_train)
+    prediction = py_pipeline.predict(predictTestData)
+    print("*********************", prediction)
+    appearances = defaultdict(int)
+    for curr in prediction:
+        appearances[curr] += 1
+    answer = max(appearances, key=appearances.get)
+    print("*********************", answer)
+    # score = py_pipeline.score(span_test_data, y)
+    context = {
+        'response': answer
+    }
+    return render(request, 'classifier_app/tindex.html', context)
 
+# from ART =======================================================================================
 
-def train(request, classifier_id):  # self, X_train, y_train, label, X_test, y_test
-    all_objects = Classifier.objects.get(id=classifier_id)
-    # all_objects = Data.objects.all()
+# def anotherFUNC(request, pk):  # self, X_train, y_train, label, X_test, y_test
+#     if request.method == 'POST':
+#         print("Posted")
+#         form = TrainDataForm(request.POST)
+#         if form.is_valid():
+#             var = request.POST['user_input']
+#             print('user input', var)
+#     else:
+#         print("didn't post")
+# 	context = {
+#         'var': var,
+#     }
+# 	return render(request, 'classifier_app/train.html', context)
 
-    X_train = all_objects.text  # comes from DB
-    label = all_objects.category    # comes from user input
+# from ART =======================================================================================
 
-    y_train = label * X_train  # passed in ( calc'd first )
-    # X_test =    # comes from user input
-    # y_test =   # passed in ( calc'd first )
-
-    py_pipeline = Pipeline([("count", CV()), ("tfid", TF()), ("multi", MNB())])
-    py_pipeline.fit(X_train, y_train)
-    # prediction = py_pipeline.predict(X_test)
-    # score = py_pipeline.score(X_test, y_test)
-    # return prediction, score
-    return py_pipeline
-
-
-def predict(request, py_pipeline, X_test):
-    prediction = py_pipeline.predict(X_test)
-    # score = py_pipeline.score(X_test, y_test)
-    return prediction
-
-
-class IndexView(TemplateView):
-    template_name = 'classifier_app/index.html'
-
-
-class TrainView(TemplateView):
-    template_name = 'classifier_app/train.html'
-
-
-class PredictView(generic.ListView):
-    template_name = 'classifier_app/predict.html'
-    context_object_name = 'data_list'
-
-    def get_queryset(self):
-        return Data.objects.all()
-
-    def get_context_data(self, **kwargs):
-        context = super(PredictView, self).get_context_data(**kwargs)
-        context['classifier_list'] = Classifier.objects.all()
-        return context
+# class IndexView(TemplateView):
+#     template_name = 'classifier_app/index.html'
+#
+#
+# class TrainView(TemplateView):
+#     template_name = 'classifier_app/train.html'
+#
+#
+# class PredictView(generic.ListView):
+#     template_name = 'classifier_app/predict.html'
+#     context_object_name = 'data_list'
+#
+#     def get_queryset(self):
+#         return Data.objects.all()
+#
+#     def get_context_data(self, **kwargs):
+#         context = super(PredictView, self).get_context_data(**kwargs)
+#         context['classifier_list'] = Classifier.objects.all()
+#         return context
 
 
 class ClassifierViewSet(viewsets.ModelViewSet):
