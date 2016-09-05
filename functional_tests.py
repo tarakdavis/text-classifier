b4c93986e3b7cb219ae5823bebcd8bdbea8e2236
Andrew Musicant <Andrewmusicant@yahoo.com>
2016-09-05 17:30:14 -0400

    added id to the predict btn

--- functional_tests.py
+++ functional_tests.py
@@ -8,17 +8,16 @@ class NewVisitorTest(unittest.TestCase):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(10)
 
-    def tearDown(self):
-        self.browser.quit()
+    # def tearDown(self):
+    #     self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://iron-classifier.herokuapp.com/')
         self.assertIn('Babel', self.browser.title)
         button = self.browser.find_element_by_id('this_is_the_predict_button')
         button.click()
-        search_input = self.browser.find_element_by_name('text_to_classif')
-        search_input.send_keys('hello world' + Keys.RETURN)
-        self.driver.getPageSource().contains("english")
+        self.browser.find_element_by_id('textArea').send_keys('hello world')
+        # self.browser.getPageSource().contains("english")
 
 
 if __name__ == '__main__':
