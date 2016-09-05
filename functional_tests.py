fdd2f5a577da09a7b892d754537d158c7adf8627
Andrew Musicant <Andrewmusicant@yahoo.com>
2016-09-05 18:22:47 -0400

    Fixed tests

--- functional_tests.py
+++ functional_tests.py
@@ -14,10 +14,12 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://iron-classifier.herokuapp.com/')
         self.assertIn('Babel', self.browser.title)
-        button = self.browser.find_element_by_id('this_is_the_predict_button')
-        button.click()
+        button1 = self.browser.find_element_by_id('this_is_the_predict_button')
+        button1.click()
         self.browser.find_element_by_id('textArea').send_keys('hello world')
-        # self.browser.getPageSource().contains("english")
+        button2 = self.browser.find_element_by_id('inputPredictBtn')
+        button2.click()
+        self.browser.getPageSource().contains("english")
 
 
 if __name__ == '__main__':
