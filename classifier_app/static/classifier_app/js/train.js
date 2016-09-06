3c049e318b81c7eff6fc566fea59387865594ebc
T. Robert Ward <trironyard@icloud.com>
2016-09-05 21:24:22 -0400

    fixed train JS to work...fianlly

--- classifier_app/static/classifier_app/js/train.js
+++ classifier_app/static/classifier_app/js/train.js
@@ -1,31 +1,47 @@
 $(document).ready(function(){
-   $('#newStuff').click(function(){
-       var clName = $('#inputclass').val()
-       var catName = $('#inputcat').val()
-       var theText = $('#textArea').val()
-       console.log(catName, theText)
-       $.ajax({
-           type: 'POST',
-           url: "/api/classifier/",
-           data: {name: clName},
-           success: function() { alert("Classifer Save Complete") },
-       })
+    $('#newStuff').click(function(){
+        var clName = $('#inputclass').val()
+        var catName = $('#inputcat').val()
+        var theText = $('#textArea').val()
+        var $flag = "NEW"
 
-       $temp = $.ajax({ url: '/api/classifier/' }).done(function(response) {
-                 $lastID = (response.length)+1
-                 var myKeyVals = {
-                   category: catName,
-                   text: theText,
-                   classifier: $lastID
-                 }
-                 $.ajax({
-                     type: 'POST',
-                     url: '/api/data/',
-                     data: myKeyVals,
-                     success: function() { alert("Data Save Complete")
-                           window.location='/'
-                       },
-                 })
-           })
-       })
+        function getClassifiers(response) {
+            return response.filter(function(result) {
+              return result.name
+            })
+        }
+
+        function checkIfAlreadyExist(response, clName, $flag) {
+            response.forEach(function(check){
+                if (check.name == clName){ $flag = String(check.id) }
+                })
+            if ($flag == "NEW" ){
+                $.ajax({
+                    type: 'POST',
+                    url: "/api/classifier/",
+                    data: {name: clName},
+                    success: function() { alert("Classifer Save Complete") },
+                        })
+            }
+            else {
+                alert ("Classifier is already in your database.")
+                    }
+            var myKeyVals = {
+                category: catName,
+                text: theText,
+                classifier: $flag
+                }
+            $.ajax({
+                  type: "POST",
+                  url: "/api/data/",
+                  data: myKeyVals,
+                  success: function() { alert("Data Save Complete")
+                        window.location='/'}
+                })
+            }
+
+        $.ajax({ url: '/api/classifier/' }).done(function(response) {
+          checkIfAlreadyExist(getClassifiers(response), clName, $flag);
+            });
+    })
 })
